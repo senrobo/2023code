324f766965e1164acdef22de2bc894ae05a3c66b
Vincent Cayadi <vincentcayadi45@gmail.com>
2023-03-12 22:26:33 +0800

    done with ultra

--- layer3-ultra/src/main.cpp
+++ layer3-ultra/src/main.cpp
@@ -1,9 +1,35 @@
-#include <Arduino.h>
-
-void setup() {
+// TODO : Add like some timeout sensor like if pinng not reviced for x duration den have 2 arrays using unions or sth like 1 is true or false if sensor is pinged
+#include "main.h"
+void setup()
+{
   // put your setup code here, to run once:
-}
+  DebugSerial.begin(15200);
+  TeenySerial.begin(15200);
+  pinMode(LED, OUTPUT);
+  digitalWrite(LED, HIGH);
 
-void loop() {
+  pinMode(ultraSensorOneTrigger, OUTPUT);
+  pinMode(ultraSensorOneEcho, INPUT);
+
+  pinMode(ultraSensorTwoTrigger, OUTPUT);
+  pinMode(ultraSensorTwoEcho, INPUT);
+
+  pinMode(ultraSensorThreeTrigger, OUTPUT);
+  pinMode(ultraSensorThreeEcho, INPUT);
+
+  pinMode(ultraSensorFourTrigger, OUTPUT);
+  pinMode(ultraSensorFourEcho, INPUT);
+}
+void loop()
+{
   // put your main code here, to run repeatedly:
+  for (int i = 0; i < 4; i++)
+  {
+    delay(50);
+    sensorDistance[i] = sonar[i].ping_cm();
+    DebugSerial.print(sensorDistance[i]);
+    DebugSerial.print(" ");
+  }
+  // Read data from teensy
+  // Send data to teensy via serial
 }
---
+++ layer3-ultra/src/main.h
@@ -0,0 +1,29 @@
+// Include Libraries
+#include <Arduino.h>
+#include <NewPing.h>
+
+// Pin Definitions
+#define LED PA15
+
+#define DebugSerial Serial1
+#define TeenySerial Serial2
+
+#define ultraSensorOneTrigger PB1
+#define ultraSensorOneEcho PB0
+#define ultraSensorTwoTrigger PA5
+#define ultraSensorTwoEcho PA4
+#define ultraSensorThreeTrigger PA6
+#define ultraSensorThreeEcho PA7
+#define ultraSensorFourTrigger PB5
+#define ultraSensorFourEcho PB6
+
+// Global Variables
+bool newData = false;
+int maxDist = 200;
+int sensorDistance[4];
+
+NewPing sonar[4] = {
+    NewPing(ultraSensorOneTrigger, ultraSensorOneEcho, maxDist),
+    NewPing(ultraSensorTwoTrigger, ultraSensorTwoEcho, maxDist),
+    NewPing(ultraSensorThreeTrigger, ultraSensorThreeEcho, maxDist),
+    NewPing(ultraSensorFourTrigger, ultraSensorFourEcho, maxDist)};
