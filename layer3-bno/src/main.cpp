718cd30c2d739801e1a947f3ba6d2d6115c290f7
Vincent Cayadi <vincentcayadi45@gmail.com>
2023-03-12 22:32:52 +0800

    fuck me pls i hate coding omfg

---
+++ layer3-bno/src/main.cpp
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
+#include <Wire.h>
+void setup()
+{
+  // put your setup code here, to run once:
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+}
