4ae9a8dfed3e5d16fea3942e2a733ef3f8efc7e4
Vincent Cayadi <vincentcayadi45@gmail.com>
2023-04-07 13:37:52 +0800

    turns out the issue was the mux being dead i thnk

--- layer1/src/main.cpp
+++ layer1/src/main.cpp
@@ -42,11 +42,24 @@ void updateSensors()
     int idx = lightCnt;
     lightVals[idx] = analogRead(mux1);
 
+    if (lightVals[idx] > fixedThreshFirstBot[idx])
+    {
+      lineDetected[outSensors] = idx;
+      outSensors++;
+    }
+
     lightCnt++;
     lightCnt %= 16;
-    
+
     idx = lightCnt + 16;
     lightVals[idx] = analogRead(mux2);
+
+    if (lightVals[idx] > fixedThreshFirstBot[idx])
+    {
+      lineDetected[outSensors] = idx;
+      outSensors++;
+    }
+
     digitalWrite(m1s0, muxChannel[lightCnt][0]);
     digitalWrite(m1s1, muxChannel[lightCnt][1]);
     digitalWrite(m1s2, muxChannel[lightCnt][2]);
@@ -59,44 +72,6 @@ void updateSensors()
     readTimer = micros();
   }
 }
-
-// void updateSensors()
-// {
-//   if (micros() - readTimer >= MUX_DELAY)
-//   {
-//     int idx = lightCnt;
-//     lightVals[idx] = analogRead(mux1);
-
-//     if (lightVals[idx] > fixedThreshFirstBot[idx])
-//     {
-//       lineDetected[outSensors] = idx;
-//       outSensors++;
-//     }
-
-//     idx = lightCnt + 15;
-
-//     lightVals[idx] = analogRead(mux2);
-//     if (lightVals[idx] > fixedThreshFirstBot[idx])
-//     {
-//       lineDetected[outSensors] = idx;
-//       outSensors++;
-//     }
-
-//     lightCnt++;
-//     lightCnt %= 16;
-
-//     // Ping via using nested lists [lightCnt] to access which mux "configuration" and [0-3] to access which pin to set [HIGH/LOW
-//     digitalWrite(m1s0, muxChannel[lightCnt][0]);
-//     digitalWrite(m1s1, muxChannel[lightCnt][1]);
-//     digitalWrite(m1s2, muxChannel[lightCnt][2]);
-//     digitalWrite(m1s3, muxChannel[lightCnt][3]);
-//     digitalWrite(m2s0, muxChannel[lightCnt][0]);
-//     digitalWrite(m2s1, muxChannel[lightCnt][1]);
-//     digitalWrite(m2s2, muxChannel[lightCnt][2]);
-//     digitalWrite(m2s3, muxChannel[lightCnt][3]);
-//     readTimer = micros();
-//   }
-// }
 
 // void processLightData()
 // {
